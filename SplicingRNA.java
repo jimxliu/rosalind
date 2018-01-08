39244037bbbeea4b43b18a7cecf190240f138ae0
Xinzhou Liu <xl6v8@mail.missouri.edu>
2018-01-08 14:16:10 -0600

    Update

--- SplicingRNA.java
+++ SplicingRNA.java
@@ -1,6 +1,11 @@
 import java.util.*;
 import java.io.*;
 
+/* After identifying the exons and introns of an RNA string, we only need to delete the introns and concatenate the exons to form a new string ready for translation.
+ * Given: A DNA string ss (of length at most 1 kbp) and a collection of substrings of ss acting as introns. All strings are given in FASTA format.
+ * Return: A protein string resulting from transcribing and translating the exons of ss. (Note: Only one solution will exist for the dataset provided.)
+ */
+
 public class SplicingRNA {
   // Codon table
   public static final Map<String,String> table;
